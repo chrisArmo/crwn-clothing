76bfb7e8105f6c7436260fa588ccbffb8d16d750
Chris Armendarez <chris.michael.armo@gmail.com>
2019-10-30 13:30:29 -0700

    Give app state & implement did mount method

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
@@ -8,21 +8,41 @@ import ShopPage from './pages/shop/shop.component';
 import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
+class App extends Component {
+  state = {
+    currentUser: null,
+  };
+  unsubscribeFromAuth = null;
 
-      <Switch>
-        <Route exact path="/" component={HomePage} />
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState(() => ({ currentUser: user }));
 
-        <Route path="/shop" component={ShopPage} />
+      console.log('User:', user);
+    });
+  }
 
-        <Route path="/sign-in-sign-up" component={SignInSignUp} />
-      </Switch>
-    </div>
-  );
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+
+          <Route path="/shop" component={ShopPage} />
+
+          <Route path="/sign-in-sign-up" component={SignInSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
